fae1b39abb4b6dcb76385ea5a03df2d0926ea72b
MaotyKevin <maotykevin@gmail.com>
2026-06-27 13:03:09 +0300

    models init

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -24,3 +24,29 @@ class Unite(models.Model):
         verbose_name_plural = "Unités"
     def __str__(self):
         return f"{self.mesure} ({self.abbreviation})"
+
+class Ingredient(models.Model):
+    idIngredient = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100, unique=True)
+
+    prixUnit = models.DecimalField(max_digits=10, decimal_places=2)
+
+    stock = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+
+    idUnite = models.ForeignKey(
+        "Unite",
+        on_delete=models.PROTECT,
+        related_name="ingredients"
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "ingredient"
+        ordering = ["nom"]
+
+    def __str__(self):
+        return f"{self.nom} ({self.stock} {self.idUnite.abbreviation})"
--- apps/menu/models.py
+++ apps/menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Plat(models.Model):
+    idPlat = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100, unique=True)
+    prixVente = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nom
+
+class PlatIngredient(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    plat = models.ForeignKey("Plat", on_delete=models.CASCADE)
+    ingredient = models.ForeignKey("Ingredient", on_delete=models.PROTECT)
+
+    quantity = models.DecimalField(max_digits=10, decimal_places=3)
+
+    class Meta:
+        db_table = "plat_ingredient"
+        unique_together = ("plat", "ingredient")
